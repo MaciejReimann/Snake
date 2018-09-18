fde619709b1dffbeb65afabdca98e56bce5db3b4
MaciejReimann <maciej.reimann@gmail.com>
2018-09-18 13:34:25 +0200

    another try

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require("express");
-const {createServer} = require('http');
 const app = express();
 
 app.use(express.static('public'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname, '/index.html');
 });
 
-const server = createServer(app);
 
 const PORT = process.env.PORT || 8080;
-
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
