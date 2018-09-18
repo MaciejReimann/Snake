b9f44beb7cf3ca57cc3155607d1abdf7292f9da7
MaciejReimann <maciej.reimann@gmail.com>
2018-09-18 04:30:53 +0200

    preparing to deploy

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
-
+const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
 const PORT = process.env.PORT || 8080;
 
