bebe292a439702f9d6138afa5fba611dd59e52ba
MaciejReimann <maciej.reimann@gmail.com>
2018-09-18 04:41:28 +0200

    now?

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const path = require('path');
 const app = express();
 
+app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname + '/public'))
 
-app.use(express.static(__dirname))
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + 'index.html'));
-});
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname + 'index.html'));
+// });
 
 const PORT = process.env.PORT || 8080;
 
