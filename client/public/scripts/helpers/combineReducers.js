f0f3cecbcd395a6e9841e0248ec3dfa4bed570f9
MaciejReimann <maciej.reimann@gmail.com>
2018-11-29 17:37:12 +0100

    combineReducers stripped down and tested

--- client/public/scripts/helpers/combineReducers.js
+++ client/public/scripts/helpers/combineReducers.js
@@ -1,8 +1,11 @@
-
-
-function combineReducers() {
-
-}
-
-
-module.exports = combineReducers;
+module.exports = function combineReducers(reducers) {
+    let combinedState = {};
+    // Leave only valid reducers, 
+    const reducersArray = Object.values(reducers)
+        .filter(reducer => typeof reducer({}) === 'object');
+    // Expose one common reducer
+    return function(state, action) {
+        reducersArray.forEach(reducer => Object.assign(combinedState, reducer(state, action)));
+        return combinedState;
+    };
+};
