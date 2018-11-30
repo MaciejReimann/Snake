d24b8636d12f7a447882526318eece85b21a8c60
MaciejReimann <maciej.reimann@gmail.com>
2018-11-30 13:18:05 +0100

    object destructuring used

--- client/public/scripts/reducers/index.js
+++ client/public/scripts/reducers/index.js
@@ -1,10 +1,12 @@
 const combineReducers = require('../helpers/combineReducers');
 const loopReducer = require('./loopReducer');
+const renderReducer = require('./renderReducer');
 const snakeReducer = require('./snakeReducer');
 const viewReducer = require('./viewReducer');
 
 module.exports = combineReducers({
-    view: viewReducer,
-    loop: loopReducer,
-    snake: snakeReducer
+    loopReducer,
+    renderReducer,
+    snakeReducer,
+    viewReducer
 })
