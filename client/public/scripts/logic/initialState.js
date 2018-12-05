cdd2bb3afb7fb8710a7b0f576b14373313d8f4b4
MaciejReimann <maciej.reimann@gmail.com>
2018-12-05 22:44:11 +0100

    initialState values changes

--- client/public/scripts/logic/initialState.js
+++ client/public/scripts/logic/initialState.js
@@ -2,12 +2,12 @@ const { RIGHT } = require('./directions').directions
 const { createPoint } = require('../helpers/pointHelpers');
 
 module.exports = {
-    tempo: 500,
+    tempo: 100,
     increaseRate: .95,
-    resolution: 20,
+    resolution: 40,
     directions: [ RIGHT ],
-    snake: Array(4).fill()
-        .map((_, i) => {return  {x: 4 - i, y: 1}   }),
+    snake: Array(3).fill()
+        .map((_, i) => {return  {x: 3 - i, y: 1}   }),
     food: createPoint(7, 1, {id: 1}),
     isOver: false,
     isStarted: false,
